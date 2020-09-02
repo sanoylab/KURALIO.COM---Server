154189b606953b489225b8af07fdb87e4aa29eb0
Yonas Yeneneh <yonasyeneneh@Yonass-MBP.phub.net.cable.rogers.com>
2020-09-02 19:57:52 -0400

    changing email message to amaric language

--- src/emails/account.js
+++ src/emails/account.js
@@ -7,14 +7,21 @@ const sendWelcomeEmail = (name, email)=>{
     sgMail.send({
         to: email,
         from:'KURALIO.ETHIOPIA@gmail.com',
-        subject: 'Welcome to KURALIO!',
+        subject: 'እንኳን ወደ ቁራልየው ዌብ ሳይት በደህና መጡ!',
         html: `
-        <table style="background-color: #fff;border-collapse:collapse; border: 1px;" width="80%" align="center">
-        <tr><td style="background-color: #373373; padding: 20px color: #fff;"><img src="https://static.cdn.responsys.net/i2/responsysimages/kijiji/contentlibrary/campaigns/welcome/fall2018/hero_en.png" width="100%"></td></tr>
-        <tr><td style="font-size:20px; line-height:1.5; "><p><strong>Hey ${name}, <br>Welcome to the Kuralio-Ethiopia community - Ethiopia's awesome online classifieds site!</strong></p></td></tr>
-        <tr><td><p>You've tapped into Ethiopia's favourite place to buy, sell and swap items and services. Now that you're here, let's kick off your Kuralio experience with some helpful tips and advice.</p></td></tr>
-        <tr><td><p>Kind regards,</p></td></tr>
-        <tr><td><p>LiVana Team</p></td></tr>
+        <table style="background-color: #fff;border-collapse:collapse; border: 1px;" width="90%" align="center">
+        <tr>
+        <td style="background-color: #373373; padding: 20px color: #fff;">
+        <img src="https://static.cdn.responsys.net/i2/responsysimages/kijiji/contentlibrary/campaigns/welcome/fall2018/hero_en.png" width="100%"></td></tr>
+        <tr>
+        <td style="font-size:20px; line-height:1.5; ">
+        <p><strong>ሰላም, <br>እንኳን ወደ ቁራልየው የቁሳቁስ ማሻሻጫ ዌብ ሳይት በደህና መጡ</strong></p>
+        </td></tr>
+        <tr><td>
+        <p>አሁን ኢትዮጵያ ውስጥ በማንኛውም ሰዓትና ቦታ የሚፈልጉትን ዕቃ መግዛት፤ መሸጥ፤ መለወጥ ይችላሉ። በቁራልዮ ዌብ ሳይት ላይ በነፃ ያስተዋውቁ፨</p>
+        </td></tr>
+        <tr><td><p>ከሰላምታ ጋር</p></td></tr>
+        <tr><td><p>የቁራልዮ ዴቨሮፐር</p></td></tr>
         </table>`
     })
 }
