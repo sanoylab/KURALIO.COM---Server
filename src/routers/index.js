f7c047dd32532e82c86864e94667664bc8d3fdf8
Yonas Yeneneh <YYeneneh@icao.int>
2021-04-18 17:15:28 -0400

    update on documentation

--- src/routers/index.js
+++ src/routers/index.js
@@ -7,13 +7,12 @@ const ads = require("./ad");
 const categories = require("./category");
 const users = require("./user");
 const messages = require('./message');
-/*
+
 router.get("/", (req, res) => {
   res.json({
     message: "AradaLej Backend API - 🥞",
   });
 });
-*/
 router.use("/ads", ads);
 router.use("/categories", categories);
 router.use("/users", users);
